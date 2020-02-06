a384fd40ed7cf0760ef8e9cfbabd4c60fdc3a17a
xdjiang <jiangxudong1982@gmail.com>
2020-02-06 19:26:01 +0800

    增加token长度校验

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 contract JCCGetPrice {
     address _owner;
-
+    uint8 constant _maxLen = 255;
     uint256 constant toWei = 10 ** 10;
     mapping(string => uint256)  _list;
 
@@ -13,7 +13,12 @@ contract JCCGetPrice {
     //设置的价格应该是实际价格*(10 ** 10),方便处理
     function _setPrice(address _setter, string _token, uint256 _price) internal returns (uint256){
         assert(_setter == _owner);
+
+        bytes memory _t = bytes(_token);
+        require(_t.length <= _maxLen);
+
         require(_price > 0);
+
         _list[_token] = _price;
         return _price;
     }
@@ -23,6 +28,9 @@ contract JCCGetPrice {
     }
 
     function getPrice(string _token) public view returns (uint256){
+        bytes memory _t = bytes(_token);
+
+        require(_t.length <= _maxLen);
         return _list[_token];
     }
 }
