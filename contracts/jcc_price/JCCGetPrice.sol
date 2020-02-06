821dd25bbec9729438a8e8644abdda41af18382e
xdjiang <jiangxudong1982@gmail.com>
2020-02-06 18:14:17 +0800

    增加外部方法

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -1,21 +1,25 @@
 pragma solidity ^0.4.24;
 
 contract JCCGetPrice {
-    address owner;
+    address _owner;
 
     uint256 constant toWei = 10 ** 10;
     mapping(string => uint256)  _list;
 
     constructor() public{
-        owner = msg.sender;
+        _owner = msg.sender;
     }
 
     //设置的价格应该是实际价格*(10 ** 10),方便处理
-    function setPrice(string _token, uint256 _price) internal returns (uint256){
-        assert(msg.sender == owner);
+    function _setPrice(address _setter, string _token, uint256 _price) internal returns (uint256){
+        assert(_setter == _owner);
         require(_price > 0);
         _list[_token] = _price;
         return _price;
+    }
+
+    function setPrice(string _token, uint256 _price) public returns (uint256){
+        return _setPrice(msg.sender, _token, _price);
     }
 
     function getPrice(string _token) public view returns (uint256){
