6a861aac14153bc4b12a1497347ecae3fedd80fe
xdjiang <jiangxudong1982@gmail.com>
2020-02-07 14:04:50 +0800

    继承Administrative，并增加event

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -1,30 +1,28 @@
 pragma solidity ^0.4.24;
 
-contract JCCGetPrice {
-    address _owner;
+import "../owner/Administrative.sol";
+
+contract JCCGetPrice is Administrative {
     uint8 constant _maxLen = 255;
     uint256 constant toWei = 10 ** 10;
     mapping(string => uint256)  _list;
 
-    constructor() public{
-        _owner = msg.sender;
-    }
+    event EnPrice(
+        string token,        
+        uint256 price);
 
     //设置的价格应该是实际价格*(10 ** 10),方便处理
-    function _setPrice(address _setter, string _token, uint256 _price) internal returns (uint256){
-        assert(_setter == _owner);
+    function _setPrice(string _token, uint256 _price) internal returns (uint256){
+        _list[_token] = _price;
+    }
 
+    function setPrice(string _token, uint256 _price)  public onlyPrivileged{ 
         bytes memory _t = bytes(_token);
         require(_t.length <= _maxLen);
 
         require(_price > 0);
-
-        _list[_token] = _price;
-        return _price;
-    }
-
-    function setPrice(string _token, uint256 _price) public returns (uint256){
-        return _setPrice(msg.sender, _token, _price);
+        _setPrice(_token, _price);
+        emit EnPrice(_token, _price);
     }
 
     function getPrice(string _token) public view returns (uint256){
