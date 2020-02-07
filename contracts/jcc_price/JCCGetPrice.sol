5d296ff309b8a62bcb28c2b1dbffa201eacba429
xdjiang <jiangxudong1982@gmail.com>
2020-02-07 17:01:38 +0800

    重构代码

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -4,31 +4,30 @@ import "../owner/Administrative.sol";
 
 contract JCCGetPrice is Administrative {
     uint8 constant _maxLen = 255;
-    uint256 constant toWei = 10 ** 10;
-    mapping(string => uint256)  _list;
+    mapping (bytes32 => uint256) _list;
 
     event EnPrice(
-        string token,        
+        string token,
         uint256 price);
 
+        constructor() Administrative() public {
+    }
     //设置的价格应该是实际价格*(10 ** 10),方便处理
-    function _setPrice(string _token, uint256 _price) internal returns (uint256){
-        _list[_token] = _price;
+    function _setPrice(bytes32 _key, uint256 _price) internal{
+        _list[_key] = _price;
     }
 
-    function setPrice(string _token, uint256 _price)  public onlyPrivileged{ 
-        bytes memory _t = bytes(_token);
-        require(_t.length <= _maxLen);
-
-        require(_price > 0);
-        _setPrice(_token, _price);
+    function setPrice(string _token, uint256 _price)  public onlyPrivileged{
+        bytes32 _key = keccak256(abi.encode(_token));
+        require(_key.length <= _maxLen, "token length too large");
+        require(_price > 0, "price must > 0");
+        _setPrice(_key, _price);
         emit EnPrice(_token, _price);
     }
 
     function getPrice(string _token) public view returns (uint256){
-        bytes memory _t = bytes(_token);
-
-        require(_t.length <= _maxLen);
-        return _list[_token];
+        bytes32 _key = keccak256(abi.encode(_token));
+        require(_key.length <= _maxLen,  "token length too large");
+        return _list[_key];
     }
 }
