8ceb29ff2ca20f496b1348b71c89651dc1a9e837
xdjiang <jiangxudong1982@gmail.com>
2020-02-08 18:57:36 +0800

    增加测试用例

---
+++ test/jcc_price/JCCGetPrice.test.js
@@ -0,0 +1,24 @@
+const JCCGetPrice = artifacts.require('JCCGetPrice');
+//const assertRevert = require('../helpers/assertRevert');
+//const zeroAccount = require('../helpers/zeroAccount');
+
+contract('JCCGetPrice', (accounts) => {
+  let PriceTest;
+  let _token = 'xrp';
+  let _price = 0.278;
+
+  it('set price by owner', async () => {
+    //assert.equal(1,1);
+    PriceTest = await JCCGetPrice.new();
+    let ret = await PriceTest.setPrice(_token, _price, { from: accounts[0] });
+    //assert.equal(1,1);
+    let { logs } = await PriceTest.setPrice(_token, _price, { from: accounts[0] });
+    let Events = logs.find(e => e.event === 'EnPrice');
+    assert.notEqual(Events, undefined);
+  });
+
+  // it('get price', async () => {
+  //   let ret = await PriceTest.getPrice(_token);
+  //   assert.equal(_price, ret);
+  // });
+});
