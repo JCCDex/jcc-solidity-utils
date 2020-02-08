2f107a2284a3a0f61eb83bdf047859284182626c
xdjiang <jiangxudong1982@gmail.com>
2020-02-09 04:43:18 +0800

    完成价格合约单元测试

--- test/jcc_price/JCCGetPrice.test.js
+++ test/jcc_price/JCCGetPrice.test.js
@@ -1,6 +1,5 @@
 const JCCGetPrice = artifacts.require('JCCGetPrice');
-//const assertRevert = require('../helpers/assertRevert');
-//const zeroAccount = require('../helpers/zeroAccount');
+const assertRevert = require('../helpers/assertRevert');
 
 contract('JCCGetPrice', (accounts) => {
   let PriceTest;
@@ -8,16 +7,20 @@ contract('JCCGetPrice', (accounts) => {
   let _price = 0.278;
 
   it('set price by owner', async () => {
-    //assert.equal(1,1);
     PriceTest = await JCCGetPrice.new();
-    //assert.equal(1,1);
-    let { logs } = await PriceTest.setPrice(_token, _price, { from: accounts[0] });
+    let { logs } = await PriceTest.setPrice(_token, web3.utils.toWei(_price + ''), { from: accounts[0] });
     let Events = logs.find(e => e.event === 'EnPrice');
     assert.notEqual(Events, undefined);
   });
 
-  // it('get price', async () => {
-  //   let ret = await PriceTest.getPrice(_token);
-  //   assert.equal(_price, ret);
-  // });
+  it('set price by not owner', async () => {
+    await assertRevert(PriceTest.setPrice(_token, web3.utils.toWei(0.3 + ''), { from: accounts[1] })); 
+  });
+
+  it('get price', async () => {
+    let ret = await PriceTest.getPrice(_token);
+    let s1 = web3.utils.fromWei(ret).toString();
+    let s2 = '' + _price;
+    assert.equal(s1,s2);
+  });
 });
