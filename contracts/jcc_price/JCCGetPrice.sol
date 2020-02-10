6760b0c2479ed795af737f72d1a97260bae75510
xdjiang <jiangxudong1982@gmail.com>
2020-02-10 19:57:49 +0800

    增加更新时间

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -3,31 +3,39 @@ pragma solidity ^0.4.24;
 import "../owner/Administrative.sol";
 
 contract JCCGetPrice is Administrative {
-    uint8 constant _maxLen = 255;
-    mapping (bytes32 => uint256) _list;
+
+  struct Price {
+    bytes32 name; //token名称
+    uint256 price;//token价格(toWei转换后的值)
+    uint time;//最后一次更新时间
+  }
+    mapping (bytes32 => Price) _list;
 
     event EnPrice(
-        string token,
+        bytes32 token,
         uint256 price);
 
         constructor() Administrative() public {
     }
     //设置的价格应该是实际价格*(10 ** 10),方便处理
-    function _setPrice(bytes32 _key, uint256 _price) internal{
-        _list[_key] = _price;
+    function _setPrice(bytes32 _key, bytes32 _token,uint256 _price) internal{
+      Price  memory _p = Price({
+        name: _token,
+        price: _price,
+        time: now
+      });
+        _list[_key] = _p;
     }
 
-    function setPrice(string _token, uint256 _price)  public onlyPrivileged{
+    function setPrice(bytes32 _token, uint256 _price)  public onlyPrivileged{
         bytes32 _key = keccak256(abi.encode(_token));
-        require(_key.length <= _maxLen, "token length too large");
         require(_price > 0, "price must > 0");
-        _setPrice(_key, _price);
+        _setPrice(_key, _token, _price);
         emit EnPrice(_token, _price);
     }
 
-    function getPrice(string _token) public view returns (uint256){
+    function getPrice(bytes32 _token) public view returns (bytes32, uint256, uint){
         bytes32 _key = keccak256(abi.encode(_token));
-        require(_key.length <= _maxLen,  "token length too large");
-        return _list[_key];
+        return (_list[_key].name, _list[_key].price, _list[_key].time);
     }
 }
--- test/jcc_price/JCCGetPrice.test.js
+++ test/jcc_price/JCCGetPrice.test.js
@@ -8,18 +8,25 @@ contract('JCCGetPrice', (accounts) => {
 
   it('set price by owner', async () => {
     PriceTest = await JCCGetPrice.new();
-    let { logs } = await PriceTest.setPrice(_token, web3.utils.toWei(_price + ''), { from: accounts[0] });
+    var _h = web3.utils.toHex(_token);
+    var _t = web3.utils.hexToBytes(_h);
+    let { logs } = await PriceTest.setPrice(_t, web3.utils.toWei(_price + ''), { from: accounts[0] });
     let Events = logs.find(e => e.event === 'EnPrice');
     assert.notEqual(Events, undefined);
   });
 
   it('set price by not owner', async () => {
-    await assertRevert(PriceTest.setPrice(_token, web3.utils.toWei(0.3 + ''), { from: accounts[1] })); 
+    var _h = web3.utils.toHex(_token);
+    var _t = web3.utils.hexToBytes(_h);
+    await assertRevert(PriceTest.setPrice(_t, web3.utils.toWei(0.3 + ''), { from: accounts[1] })); 
   });
 
   it('get price', async () => {
-    let ret = await PriceTest.getPrice(_token);
-    let s1 = web3.utils.fromWei(ret).toString();
+    var _h = web3.utils.toHex(_token);
+    var _t = web3.utils.hexToBytes(_h);
+    console.log(_h);
+    let ret = await PriceTest.getPrice(_t);
+    let s1 = web3.utils.fromWei(ret[1]).toString();
     let s2 = '' + _price;
     assert.equal(s1,s2);
   });
