1457beb40076c0904ee929435f02802408366cf7
xdjiang <jiangxudong1982@gmail.com>
2020-02-12 14:39:21 +0800

    还原合约接口的参数类型

--- contracts/jcc_price/JCCGetPrice.sol
+++ contracts/jcc_price/JCCGetPrice.sol
@@ -1,41 +1,50 @@
 pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
 
 import "../owner/Administrative.sol";
 
 contract JCCGetPrice is Administrative {
 
+  uint8 constant _maxLen = 20;
   struct Price {
-    bytes32 name; //token名称
-    uint256 price;//token价格(toWei转换后的值)
+    string symbol; //名称
+    uint256 price;//价格(toWei转换后的值)
     uint time;//最后一次更新时间
   }
     mapping (bytes32 => Price) _list;
 
     event EnPrice(
-        bytes32 token,
+        string symbol,
         uint256 price);
 
         constructor() Administrative() public {
     }
     //设置的价格应该是实际价格*(10 ** 10),方便处理
-    function _setPrice(bytes32 _key, bytes32 _token,uint256 _price) internal{
+    function _setPrice(bytes32 _key, string _symbol,uint256 _price) internal{
       Price  memory _p = Price({
-        name: _token,
+        symbol: _symbol,
         price: _price,
         time: now
       });
         _list[_key] = _p;
     }
 
-    function setPrice(bytes32 _token, uint256 _price)  public onlyPrivileged{
-        bytes32 _key = keccak256(abi.encode(_token));
+    function setPrice(string _symbol, uint256 _price)  public onlyPrivileged{
+        bytes memory _t = bytes(_symbol);
+        require(_t.length > 0, 'symbol length must not null');
+        require(_t.length <= _maxLen, 'symbol length too large');
         require(_price > 0, "price must > 0");
-        _setPrice(_key, _token, _price);
-        emit EnPrice(_token, _price);
+        bytes32 _key = keccak256(abi.encodePacked(_symbol));
+
+        _setPrice(_key, _symbol, _price);
+        emit EnPrice(_symbol, _price);
     }
 
-    function getPrice(bytes32 _token) public view returns (bytes32, uint256, uint){
-        bytes32 _key = keccak256(abi.encode(_token));
-        return (_list[_key].name, _list[_key].price, _list[_key].time);
+    function getPrice(string _symbol) public view returns (string, uint256, uint){
+        bytes memory _t = bytes(_symbol);
+        require(_t.length > 0, 'symbol length must not null');
+        require(_t.length <= _maxLen, 'symbol length too large');
+         bytes32 _key = keccak256(abi.encodePacked(_symbol));
+        return (_list[_key].symbol, _list[_key].price, _list[_key].time);
     }
 }
--- test/jcc_price/JCCGetPrice.test.js
+++ test/jcc_price/JCCGetPrice.test.js
@@ -3,31 +3,31 @@ const assertRevert = require('../helpers/assertRevert');
 
 contract('JCCGetPrice', (accounts) => {
   let PriceTest;
-  let _token = 'xrp';
+  let _token = 'xrpusdt';
   let _price = 0.278;
 
   it('set price by owner', async () => {
     PriceTest = await JCCGetPrice.new();
-    var _h = web3.utils.toHex(_token);
-    var _t = web3.utils.hexToBytes(_h);
-    let { logs } = await PriceTest.setPrice(_t, web3.utils.toWei(_price + ''), { from: accounts[0] });
+    let { logs } = await PriceTest.setPrice(_token, web3.utils.toWei(_price + ''), { from: accounts[0] });
     let Events = logs.find(e => e.event === 'EnPrice');
     assert.notEqual(Events, undefined);
   });
 
   it('set price by not owner', async () => {
-    var _h = web3.utils.toHex(_token);
-    var _t = web3.utils.hexToBytes(_h);
-    await assertRevert(PriceTest.setPrice(_t, web3.utils.toWei(0.3 + ''), { from: accounts[1] })); 
+    await assertRevert(PriceTest.setPrice(_token, web3.utils.toWei(0.3 + ''), { from: accounts[1] })); 
   });
 
   it('get price', async () => {
-    var _h = web3.utils.toHex(_token);
-    var _t = web3.utils.hexToBytes(_h);
-    console.log(_h);
-    let ret = await PriceTest.getPrice(_t);
+    let ret = await PriceTest.getPrice(_token);
     let s1 = web3.utils.fromWei(ret[1]).toString();
     let s2 = '' + _price;
+    assert.equal(s1,s2);
+  });
+
+  it('get price, token not exit', async () => {
+    let ret = await PriceTest.getPrice('ethusdt');
+    let s1 = web3.utils.fromWei(ret[1]).toString();
+    let s2 = '' + 0;
     assert.equal(s1,s2);
   });
 });
